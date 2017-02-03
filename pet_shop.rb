3fe5fc3449a25698aa527f660bfa2a400739bd5d
Rob <robflettcc@gmail.com>
2017-02-03 19:26:30 +0000

    Third method completed

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,3 +6,7 @@ def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] += cash
+  return pet_shop[:admin][:total_cash]
+end
