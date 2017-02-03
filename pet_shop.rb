db28a145f9edc179e8d2bc3c37a9d284843caab3
Rob <robflettcc@gmail.com>
2017-02-03 19:37:41 +0000

    5th method - returning number of pets sold

--- pet_shop.rb
+++ pet_shop.rb
@@ -15,3 +15,7 @@ def add_or_remove_cash(pet_shop, cash)
   pet_shop[:admin][:total_cash] += cash
   return pet_shop[:admin][:total_cash]
 end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
