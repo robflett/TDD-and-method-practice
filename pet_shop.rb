8c9db22d272378b4ee27cfd47d77e2b13233f891
Rob <robflettcc@gmail.com>
2017-02-05 10:50:02 +0000

    all_pets method added - for found and not found

--- pet_shop.rb
+++ pet_shop.rb
@@ -28,3 +28,31 @@ end
 def stock_count(pet_shop)
   return pet_shop[:pets].length
 end
+
+def pets_by_breed(pet_shop, dog_type)
+  array_of_pets = []
+
+  for pet in pet_shop[:pets]
+    if pet.has_value?(dog_type)
+      array_of_pets << pet
+    end
+  end
+
+  return array_of_pets
+
+end           
+
+# def pets_by_breed(pet_shop, dog_type)
+#   array_of_pets = []
+
+#   for item in pet_shop[:pets]
+
+#     if array_of_pets(dog_type)
+#       array_of_pets(dog_type).push(item[:pets][:breed])
+#     end
+
+#   end 
+
+#   return array_of_pets
+#end            #trying to get an array that returns 2 to match the expected value. 2 refers to the amount of British Shorthairs - how to get the shorthairs to be counted but not the others??? If British Shorthairs == nil then delete that entry?
+#The test is looking for 2 in the pets variable.
